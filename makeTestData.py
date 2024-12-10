15246145acfa46454451632effd10d98250c6837
samm4444 <39239211+samm4444@users.noreply.github.com>
2024-12-10 12:53:05 +0000

    altered makeTestData.py to be easier to use

--- makeTestData.py
+++ makeTestData.py
@@ -4,24 +4,37 @@ import os
 import subprocess
 import arguably
 import math
+from getpass import getpass
 
 @arguably.command
-def create(records: int):
+def create(outputDB, records: int):
   '''
   Create some mock data
 
   Args:
+    outputDB (str): The address for the output database. Format: host:database:table
     records (int): Number of records to create (will be rounded up to 5k)
   '''
   filename = "insert.sql"
 
   open(filename, 'w').close()
 
+  outputDBdata = outputDB.split(":")
+  try:
+      outputDBHost = outputDBdata[0]
+      outputDatabaseName = outputDBdata[1]
+      outputDBTable = outputDBdata[2]
+  except IndexError:
+      return
+  # get username and password input for db
+  outputDBUsername = input("Username:")
+  outputDBPassword = getpass()
+
   connection = mysql.connector.connect(
-      host="localhost",
-      user="hc24-25",
-      password="password",
-      database="hc24_25"
+      host=outputDBHost,
+      user=outputDBUsername,
+      password=outputDBPassword,
+      database=outputDatabaseName
   )
   
   n = math.ceil((records / 5000))
